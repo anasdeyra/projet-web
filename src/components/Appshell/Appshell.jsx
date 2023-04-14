9dae16b2fa1db34009b66b8fc005a2d164415562
mohamed amine tebbini <tebbinimohamedamine@gmail.com>
2023-04-14 17:36:16 +0100

    hama's commit

--- src/components/Appshell/Appshell.jsx
+++ src/components/Appshell/Appshell.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Navbar from "../navbar/Navbar";
+import Footer from "../footer/Footer";
 
 export default function Appshell({ children }) {
   return (
