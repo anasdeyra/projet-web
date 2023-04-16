84eda06100aabcfb3a826d3e3b344b0132121293
Anass <45261478+anasdeyra@users.noreply.github.com>
2023-04-16 15:29:52 +0100

    added footer and links

--- src/components/pages/index/Cta2.jsx
+++ src/components/pages/index/Cta2.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Cta2() {
   return (
-    <div className="max-w-7xl mx-auto relative mt-40 p-10 rounded-xl bg-gradient-to-tr from-purple-600 to-pink-600 flex justify-between items-center">
+    <div className="max-w-7xl mx-auto relative mt-40 p-10 rounded-xl bg-gradient-to-tr from-purple-700 to-pink-600 flex justify-between items-center">
       <div>
         <h2 className="font-bold text-3xl">Reach Out to Us</h2>
         <h3 className="font-semibold text-xl mt-2">
           We value your feedback and are always happy to hear from our users
         </h3>
       </div>
-      <button className="bg-white text-neutral-800 rounded-lg px-4 py-3 font-semibold transition-all active:scale-[0.98]">
+      <a
+        href={"/contact-us"}
+        className="bg-white text-neutral-800 rounded-lg px-4 py-3 font-semibold transition-all active:scale-[0.98]"
+      >
         Contact Us
-      </button>
+      </a>
       <img
         src="/cta2.png"
         alt="cta"
--- src/components/pages/index/Hero.jsx
+++ src/components/pages/index/Hero.jsx
@@ -13,12 +13,18 @@ export default function Hero() {
         </h2>
       </div>
       <div className="flex items-center justify-center gap-8 mt-10 mx-auto">
-        <button className="text-lg text-neutral-950 font-semibold bg-gradient-to-bl hover:bg-green-500 from-emerald-700 to-green-500 px-5 py-2 rounded-full transition-all active:scale-[0.98]">
+        <a
+          href={"/services"}
+          className="text-lg text-neutral-950 font-semibold bg-gradient-to-bl hover:bg-green-500 from-emerald-700 to-green-500 px-5 py-2 rounded-full transition-all active:scale-[0.98]"
+        >
           Join now
-        </button>
-        <button className="flex gap-1 items-center transition-all active:scale-[0.98]">
+        </a>
+        <a
+          href={"/about-us"}
+          className="flex gap-1 items-center transition-all active:scale-[0.98]"
+        >
           Learn more <FiArrowRight />
-        </button>
+        </a>
       </div>
     </div>
   );
