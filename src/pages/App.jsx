fa1ed24c2bc2954b4588b090262379efc8c0f4d5
Anass <45261478+anasdeyra@users.noreply.github.com>
2023-04-19 20:08:15 +0100

    merge

---
+++ src/pages/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from '../components/pages/apppage/Home'
+import AppLayout from '../components/pages/apppage/AppLayout'
+const App = () => {
+  return (
+   <AppLayout>
+    <Home/>
+   </AppLayout>
+  )
+}
+
+export default App
---
+++ src/pages/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppLayout from '../components/pages/apppage/AppLayout'
+
+const Search = () => {
+  return (
+    <AppLayout>
+        <div> 
+            hey
+            </div>
+    </AppLayout>
+  )
+}
+
+export default Search
